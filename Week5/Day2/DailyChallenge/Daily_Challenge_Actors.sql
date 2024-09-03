2a886d0d5a17ae505457e7c1fb964c71b3d7d28f
Elikinde <sagels259@gmail.com>
2024-09-03 22:54:35 +0300

    sql Daily Challenge

---
+++ Week5/Day2/DailyChallenge/Daily_Challenge_Actors.sql
@@ -0,0 +1,8 @@
+-- SELECT COUNT(*) FROM actors;
+
+-- INSERT INTO actors (first_name, last_name, birthdate, number_iscars)
+-- VALUES ('Makes', , '02/05/1999', 5)
+
+-- ERROR:  syntax error at or near ","
+-- LINE 4: VALUES ('Makes', , '02/05/1999', 5)
+--                          ^ 
