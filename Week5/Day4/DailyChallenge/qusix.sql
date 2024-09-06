64da3566bca66e73c524c7549d448bd1fa6a1e4c
Elikinde <sagels259@gmail.com>
2024-09-06 08:43:04 +0300

    function matrix

---
+++ Week5/Day4/DailyChallenge/qusix.sql
@@ -0,0 +1,12 @@
+CREATE TABLE FirstTab (
+     id integer, 
+     name VARCHAR(10)
+);
+
+INSERT INTO FirstTab VALUES
+(5,'Pawan'),
+(6,'Sharlee'),
+(7,'Krish'),
+(NULL,'Avtaar');
+
+SELECT * FROM FirstTab;
