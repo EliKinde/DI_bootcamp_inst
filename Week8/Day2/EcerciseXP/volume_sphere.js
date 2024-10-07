150a90ae577f5fb2148af489946794653b63ee22
Elikinde <sagels259@gmail.com>
2024-10-07 20:46:46 +0300

    delete some duplicate

---
+++ Week8/Day2/EcerciseXP/volume_sphere.js
@@ -0,0 +1,6 @@
+let radius = document.getElementById('radius');
+let volume = document.getElementById('volume');
+
+let radius_value = Number(radius.value);
+let calcualted_volume;
+console.log((4/3)*3.14159*(Number(radius.value))**3);
